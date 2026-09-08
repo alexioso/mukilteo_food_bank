083224a832510636e19708a395982ccfa1786e3a
alexioso <alexbraksator@gmail.com>
2026-09-07 19:10:46 -0700

    Update main_refresh.sh

--- main_refresh.sh
+++ main_refresh.sh
@@ -4,6 +4,9 @@ cd src
 python mandatory_report_refresh.py
 python generate_forecast.py
 #git add ../data/prep/match_stats.csv
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@://github.com"
+
 git add ../data/*
 git commit -m "refresh data $(date +%Y-%m-%d)"
 git push
