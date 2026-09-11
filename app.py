070392c3d9ce57fd760e30476bc99608a53ecfb2
Alex Braksator <alexbraksator@Alexs-MacBook-Air.local>
2026-09-10 19:42:04 -0700

    minor updates

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="MFB Distribution Dashboard", layout="wide")
 
-visuals = st.Page("pages/visuals.py", title="Visuals", default=True)
-month_end_summary = st.Page("pages/month_end_summary.py", title="Month End Summary")
+month_end_summary = st.Page("pages/month_end_summary.py", title="Month End Summary", default=True)
+visuals = st.Page("pages/visuals.py", title="Visuals")
 forecast = st.Page("pages/forecast.py", title="Forecast")
 
-pg = st.navigation([visuals, month_end_summary, forecast])
+pg = st.navigation([month_end_summary, visuals, forecast])
 pg.run()
