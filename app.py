5124fb7ac593be934050e55f12f47b83584fb4d3
Alex Braksator <alexbraksator@Alexs-MacBook-Air.local>
2026-09-10 20:07:19 -0700

    changed browserbase api key to scret

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="MFB Distribution Dashboard", layout="wide")
 month_end_summary = st.Page("pages/month_end_summary.py", title="Month End Summary", default=True)
 visuals = st.Page("pages/visuals.py", title="Visuals")
 forecast = st.Page("pages/forecast.py", title="Forecast")
+about = st.Page("pages/about.py", title="About")
 
-pg = st.navigation([month_end_summary, visuals, forecast])
+pg = st.navigation([month_end_summary, visuals, forecast, about])
 pg.run()
